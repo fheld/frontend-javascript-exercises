ecc11f8335c1e06377142d95753dc14b7d2feb53
Forrest Held <forrest.held@comcast.net>
2015-04-06 19:09:43 +0000

    Answers for 05 Booleans

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,19 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+   return stringOne === stringTwo;
 };
 
 module.exports.notEqual = function(one, two) {
-
+  return one !== two;
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+      return middle > lower && middle < upper;
 };
 
 module.exports.outsideRanges = function(number) {
-
+  return (number < 10 || number > 20) && (number < 43 || number > 75) && (number < 2 || number > 5);
 };
 
 module.exports.nameAndPrice = function(name, price) {
-
+  return (name == 'NYTimes' || name == 'LATimes') && (price >= 1);
 };

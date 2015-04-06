674841665bbcec2a64d081f7205dbbd506b6713f
Forrest Held <forrest.held@comcast.net>
2015-04-06 15:34:49 +0000

    Answers to 04-Strings

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,19 +1,24 @@
 module.exports.formLetter = function(firstName, senderName, message) {
-
+  return    "Hello " + firstName + ",\n\n" + message + "\n\n"  + "Sincerely,\n" +senderName ;
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
-
+  return bigString.substring(startA, endA) + bigString.substring(startB, endB);
 };
 
 module.exports.findFirstMatch = function(text, searchString) {
-
+  return text.indexOf(searchString)
 };
 
 module.exports.findLastMatch = function(text, searchString) {
-
+ return text.lastIndexOf(searchString);
 };
 
 module.exports.substringBetweenMatches = function(text, searchString) {
-
+   var length = searchString.length;
+   var start = text.indexOf(searchString) + length ;
+   var last = text.lastIndexOf(searchString) ;
+   return text.substring(start, last);
 };
+
+ 

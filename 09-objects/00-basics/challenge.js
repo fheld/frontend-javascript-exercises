3f19419fe089cd7c5acef8c79407711d93022495
Forrest Held <forrest.held@comcast.net>
2015-04-07 23:51:05 +0000

    Answers for 09 Objects

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,33 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents){
+ var courseInfo = { title:courseTitle,
+                    duration:courseDuration,
+                    students:courseStudents};
 
-module.exports.addProperty = undefined;
+   return courseInfo;
 
-module.exports.formLetter = undefined;
+};
 
-module.exports.canIGet = undefined;
+module.exports.addProperty = function(object, newProp, newValue){
+  if(newProp in object){
+   return object;
+ }
+ else{
+    object[newProp] = newValue;
+    return object;  
+ };
+
+};
+
+module.exports.formLetter = function(letter){
+ return  "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+};
+
+module.exports.canIGet = function(item, money){
+    var apple =  {'MacBook Air': 999,
+                  'MacBook Pro': 1299,
+                  'Mac Pro': 2499,
+                  'Apple Sticker': 1                 
+                };
+
+           return (apple[item] <= money);          
+};

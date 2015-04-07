eaa6d25c0009ae4d2f680f8526588e4212c5718c
Forrest Held <forrest.held@comcast.net>
2015-04-07 02:09:05 +0000

    Answers for 07 If Statements

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,5 +1,32 @@
-module.exports.favoriteNumber = undefined;
+module.exports.favoriteNumber = function(favorite, guess){
+  if(guess > favorite) {
+    return "Too high"; 
+  } else if( guess < favorite){
+    return "Too low";
+  } else {
+    return "You got it!";
+  }
+};
 
-module.exports.checkLock = undefined;
+module.exports.checkLock = function(num1, num2, num3, num4){
+  if((num1 == 3 || num1 == 5 || num1 == 7) && (num2 == 2) && (num3 > 4 && num3 < 101) && (num4 < 9 || num4 > 20)) {
+    return "correct";
+  } else {
+    return "incorrect";
+  }
+    
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money){
+  if(item == "MacBook Air" && money >= 999){
+    return true;
+  } else if(item == "MacBook Pro" && money >= 1299){
+    return true;
+  } else if(item == "Mac Pro" && money >= 2499){
+    return true;
+  } else if(item == "Apple Sticker" && money >= 1){
+    return true;
+  } else {
+    return false;
+  }
+};
